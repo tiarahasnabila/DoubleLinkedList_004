d72d18db74315d5c0d55247e43166b3de3830885
tiarahasnabila <tiarahasnabila@gmail.com>
2023-05-25 15:22:12 +0700

    membuat double link dimulai dari start

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -9,4 +9,23 @@ public:
 	Node* next;
 	Node* prev;
 };
+class DoubleLinkedList {
+private:
+	Node* START;
+public:
+
+	DoubleLinkedList();
+	void addNode();
+	bool search(int rollNo, Node** previous, Node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void ascending();
+	void descending();
+	void hapus();
+	void searchData();
+};
+
+DoubleLinkedList::DoubleLinkedList() {
+	START = NULL;
+}
 
