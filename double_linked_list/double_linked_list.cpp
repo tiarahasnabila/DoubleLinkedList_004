0e1380595b3fc306e354e9715ba6db58ace19431
tiarahasnabila <tiarahasnabila@gmail.com>
2023-05-25 15:21:24 +0700

    membuatt class node

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -10,18 +10,3 @@ public:
 	Node* prev;
 };
 
-class DoubleLinkedList {
-private:
-	Node* START;
-public:
-
-	DoubleLinkedList();
-	void addNode();
-	bool search(int rollNo, Node** previous, Node** current);
-	bool deleteNode(int rollNo);
-	bool listEmpty();
-	void ascending();
-	void descending();
-	void hapus();
-	void searchData();
-};
