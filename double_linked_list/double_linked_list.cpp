bcc087ca867d1ec09cf37d4f3c5f7b6ad58d4fb2
tiarahasnabila <tiarahasnabila@gmail.com>
2023-05-25 15:24:04 +0700

    jika start = null maka melanjutkan ke node selanjutnya

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -41,4 +41,17 @@ void DoubleLinkedList::addNode() {
 	newNode->noMhs = nim;
 	newNode->name = nm;
 
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START;
+		if (START != NULL)
+			START->prev = newNode;
+		newNode->prev = NULL;
+		START = newNode;
+		return;
+	}
+
 	
